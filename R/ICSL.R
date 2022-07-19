b34854a326c7214bef0b88d78f4263c66fd8abc0
moondog1969 <108488084+moondog1969@users.noreply.github.com>
2022-07-19 10:50:11 -0600

    Add files via upload

---
+++ R/ICSL.R
@@ -0,0 +1,35 @@
+ICSL <- function(G, coords = NULL, names = NULL, lengths = NULL, dist.matrix = NULL, show.dist = FALSE){
+  if(is.null(coords) & is.null(length)) stop("One of coords or arc lengths must be specfied")
+  
+  a <- attributes(A(G))$vnames
+  bounds <- matrix(ncol = 1, data = unlist(strsplit(a, "\\|")), byrow = TRUE)  
+  
+  if(is.null(lengths) & is.null(dist.matrix)){
+    row.names(coords) <- names
+    dists <- dist(coords)
+    m <- match(row.names(coords), unique(bounds))
+    rn <- row.names(coords)[!is.na(m)]
+    w <- which(row.names(coords) %in% rn)
+    d <- dist(coords[w,])
+    icsl <- mean(d)
+  }
+  if(!is.null(lengths) & is.null(dist.matrix))  {
+  E(G)$weight <- lengths
+  d <- distances(G) 
+  d2 <- ifelse(d == Inf, NA, d)
+  icsl <- mean(d2[upper.tri(d2)], na.rm = TRUE)
+  }
+  if(!is.null(dist.matrix))  {
+  if (!inherits(dist.matrix, "dist")) stop("dist.matrix must be of class dist")
+  d <- dist.matrix
+  cn <- colnames(as.matrix(d))
+  w <- which(cn %in% attributes(V(G))$name)
+  d <- as.matrix(d)[w,][,w]
+  d2 <- ifelse(d == Inf, NA, d)
+  d2 <- as.dist(d2)
+  icsl <- mean(d2[upper.tri(d2)], na.rm = TRUE)
+  }
+  
+  if(show.dist) list(iscl = icsl, distance.matrix = d) else icsl  
+}
+
---
+++ R/multi.path.visibility.R
@@ -0,0 +1,50 @@
+multi.path.visibility <- function(G, degree = "in", source = NULL, sink = NULL, directed = TRUE, weights = NULL){
+
+  list.paths <- list(1:length(source))
+  list.names <- list(1:length(source))
+  list.t <- list(1:length(source))
+  for(i in 1:length(source)){ 
+    out <- path.visibility(G, degree = degree, source = source[i], sink = sink, directed = directed)
+    list.paths[[i]] <- out
+    list.names[[i]] <- colnames(out)
+    list.t[[i]] <- data.frame(Names = colnames(out), Time = 1:length(colnames(out)))
+  }
+  
+  time.matrix <- list.t[[1]]
+  for(i in 2:length(list.t)){
+    temp <- rbind(time.matrix, list.t[[i]])
+    time.matrix <- temp
+  }  
+  
+  v <- list.paths  
+  time.summary <- sort(tapply(time.matrix[,2], time.matrix[,1], sum))
+  ts.mat <- matrix(FALSE, ncol = length(time.summary), nrow = length(time.summary), dimnames = list(names(time.summary), names(time.summary)))
+  
+  
+  for(i in 1:length(v)){
+    one <- v[[i]]
+    w <- which(colnames(ts.mat) %in% colnames(one))
+    ts.mat[,w][w,] <- one
+  }
+  
+  list.out <- list()
+  # list.out$visibilities <- list.paths
+  # list.out$names <- unique(unlist(list.names))
+  list.out$all.visibilities <- ts.mat
+  
+  fr <- apply(ts.mat, 1, function(x)sum(x,na.rm=T))
+  to <- apply(ts.mat, 2, function(x)sum(x,na.rm=T))
+  sum <- to + fr
+  summary <- rbind(fr,to,sum)
+  row.names(summary) <- c("out", "in", "both")
+  if(!is.null(weights)){ if(all(sort(row.names(weights)) == sort(row.names(summary)))) stop("row names in weights must correspond to node names in G")
+    m <- match(colnames(summary), row.names(weights))
+    out <- summary
+    for(i in 1:3){
+      out[i,] <- summary[i,] * weights[,1][m] 
+    }
+    summary <- out
+  }
+  list.out$visibility.summary <- summary
+  list.out
+}
