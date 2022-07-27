9b8056172435aaf7b5753dcbcbf876cc7d8a81c9
moondog1969 <108488084+moondog1969@users.noreply.github.com>
2022-07-27 10:27:42 -0600

    Delete R directory

--- R/pinvbeta.R
+++
@@ -1,9 +0,0 @@
-#--- CDF ---#
-
-pinvbeta <- function(x, alpha, beta){
-  integrand <-  function(x){
-    gamma(alpha + beta)/(gamma(alpha)*gamma(beta)) *
-      (1/x)^(alpha + 1) * (1 - 1/x)^(beta - 1)}
-  area <- integrate(integrand, 1, x)
-  area$value
-}
