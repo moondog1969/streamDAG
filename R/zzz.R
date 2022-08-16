1ccf3e47bd15035fd714924d805a2da99323a466
moondog1969 <108488084+moondog1969@users.noreply.github.com>
2022-08-16 14:13:00 -0600

    Delete R directory

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-"\nA reminder: ancillary data entries corresponding to stream DAG arcs\nand nodes (e.g., weights and other graph modifiers) should have\nthe same ordering as the arcs or nodes in the stream DAG igraph\nobject that the ancillary data correspond to. The order of nodes\nand arcs in an igraph object, G, can be checked using V(G) and E(G),\nrespectively. For more information type:\n\nvignette(\"streamDAG.examples\", package = \"streamDAG\")", appendLF = FALSE)
-}
