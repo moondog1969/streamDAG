f1f8357bf157db01cd278b376349f99f09dfee6a
moondog1969 <108488084+moondog1969@users.noreply.github.com>
2022-09-01 14:02:37 -0600

    Add files via upload

---
+++ R/intact_to_sink.R
@@ -0,0 +1,14 @@
+size.intact.to.sink <- function(G, node = NULL){
+  groups <- components(G, "weak")
+  members <- groups$membership
+  w <- which(names(members) == node)
+  node.group <- which(members == members[w])
+  node.members <- attributes(V(G))$names[node.group]
+  sG <- subgraph(G, node.members)
+  
+  if(is.null(E(G)$weights)){length <- ecount(sG)}
+  else {length <- sum(E(sG)$weights)} 
+  length
+}
+
+
